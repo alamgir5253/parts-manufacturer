f27f6d97f9917b8996bad115cd0cbbef46033deb
alomgir hossain <alamgir5253@.com>
2022-05-24 02:37:28 +0600

    connected to mongodb done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000
 const app = express()
 // middle ware 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r8qzj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+console.log(uri);
+
 
 async function run() {
   try {
@@ -23,7 +35,7 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send(' process successssss')
+  res.send(' process success')
 })
 app.listen(port, () => {
   console.log('listening to portt', port);
